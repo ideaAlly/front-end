ed7255984ae5b94989e50433d5615161b38b2dae
shamanth a r <shamanthar2000@gmail.com>
2021-04-23 15:51:35 +0530

    'First'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './interest/Navbar.css'
+import Navbar from './interest/Navbar'
+import Box from './interest/Box'
+import './interest/Box.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Box></Box>
     </div>
   );
 }
