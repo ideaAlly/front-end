7f2566915a108fb80155b7cfb22ed47479773e9e
shamanth a r <shamanthar2000@gmail.com>
2021-05-04 13:02:51 +0530

    Second Commit

--- src/interest/Box.js
+++ src/interest/Box.js
@@ -1,21 +1,51 @@
 import React from "react";
-import './Click.js'
+
 
 function Box() {
+	var Selected = [];
+	var Saved = false;
+	//Function for the Save button goes here!!!
+	/* 
+		function Save(){
+			Update the selected array to the back-end;
+			Make the Saved Variable as true;
+		}	
+	*/
+	if (Saved == true) {
+		//Function for the Next button goes here!!!
+		/*
+			function Next(){
+				Link the href to the main page;
+			}
+		*/
+	}
+	function ClickMe(BtnName) {
+		var Button = document.getElementById(BtnName);
+		if (Button.State == "0") {
+			Button.style.backgroundColor = "cadetblue";
+			Button.style.color = "White";
+			Button.State = "1";
+			Selected.push(BtnName);
+			console.log(Selected);
+		}
+		else {
+			Button.style.backgroundColor = 'white';
+			Button.style.color = "black";
+			Button.State = "0";	
+			var index = Selected.indexOf(BtnName);
+			if (index > -1) {
+				Selected.splice(index, 1);
+			}
+			console.log(Selected);
+		}
+		
+
+	}
 	return (
 		<>
 			<div className="outer">
 				<div className="top-part">
 					<h3 id="top-text">Choose your Interests</h3>
-					<div className="Search">
-						<input
-							type="text"
-							placeholder="Search your Domains here..."
-						></input>
-					</div>
-					<div className="container">
-						<button className="btn btn1">Search</button>
-					</div>
 				</div>
 				<div className="line"></div>
 				<div className="Guide">
@@ -26,97 +56,206 @@ function Box() {
 						<button
 							className="Domains"
 							id="Artificial Intelligence"
-							onClick="Click('Artificial Intelligence')"
+							State="1"
+							onClick={() => {
+								ClickMe("Artificial Intelligence");
+							  }}
 						>
 							Artificial Intelligence
 						</button>
-						<button className="Domains" value="Machine Learning" onClick="Click('Machine Learning')" id="Machine Learning">
+						<button className="Domains" id="Machine Learning"
+							State="0"
+							onClick={() => {
+								ClickMe("Machine Learning");
+							  }}>
 							Machine Learning
 						</button>
 
-						<button className="Domains" value="Web Development" onClick="">
+						<button className="Domains" id="Web Development"
+							State="0"
+							onClick={() => {
+								ClickMe("Web Development");
+							  }}>
 							Web Development
 						</button>
 						<button
 							className="Domains"
-							value="Natural Language Processing"
-							onClick=""
+							State="0"
+							id="Natural Language Processing"
+							onClick={() => {
+								ClickMe("Natural Language Processing");
+							  }}
 						>
 							Natural Language Processing
 						</button>
 
-						<button className="Domains" value="Deep Learining" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Deep Learining"
+							onClick={() => {
+								ClickMe("Deep Learining");
+							  }}
+						>
 							Deep Learning
 						</button>
 					</div>
 
 					<div className="below-inner-2">
-						<button className="Domains" value="Big data Analytics" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Big data Analytics"
+							onClick={() => {
+								ClickMe("Big data Analytics");
+							  }}
+						>
 							Big data Analytics
 						</button>
-						<button className="Domains" value="Bioinformatics" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Bioinformatics"
+							onClick={() => {
+								ClickMe("Bioinformatics");
+							  }}
+						>
 							Bioinfirmatiics
 						</button>
 
-						<button className="Domains" value="Cyber Security" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Cyber Security"
+							onClick={() => {
+								ClickMe("Cyber Security");
+							  }}
+						>
 							Cyber Security
 						</button>
-						<button className="Domains" value="Cloud Computing" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Cloud Computing"
+							onClick={() => {
+								ClickMe("Cloud Computing");
+							  }}
+						>
 							Cloud Computing
 						</button>
 
 						<button
 							className="Domains"
-							value="Mobile Application development"
-							onClick=""
+							State="0"
+							id="Mobile Application development"
+							onClick={() => {
+								ClickMe("Mobile Application development");
+							  }}
 						>
 							Mobile Application Development
 						</button>
 					</div>
 
 					<div className="below-inner-3">
-						<button className="Domains" value="Data Sciece" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Data Sciece"
+							onClick={() => {
+								ClickMe("Data Sciece");
+							  }}
+						>
 							Data Science
 						</button>
-						<button className="Domains" value="Database Management" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Database Management"
+							onClick={() => {
+								ClickMe("Database Management");
+							  }}
+						>
 							Database Management
 						</button>
 
-						<button className="Domains" value="Computer Networks" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Computer Networks"
+							onClick={() => {
+								ClickMe("Computer Networks");
+							  }}
+						>
 							Computer Networks
 						</button>
-						<button className="Domains" value="Virtual Reality" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Virtual Reality"
+							onClick={() => {
+								ClickMe("Virtual Reality");
+							  }}
+						>
 							Virtual Reality
 						</button>
 
-						<button className="Domains" value="Augmented Reality" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Augmented Reality"
+							onClick={() => {
+								ClickMe("Augmented Reality");
+							  }}>
 							Augmented Reality
 						</button>
 					</div>
 
 					<div className="below-inner-4">
-						<button className="Domains" value="Internet Of Things" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Internet Of Things"
+							onClick={() => {
+								ClickMe("Internet Of Things");
+							  }}
+						>
 							Internet Of Things
 						</button>
-						<button className="Domains" value="Blockchain" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Blockchain"
+							onClick={() => {
+								ClickMe("Blockchain");
+							  }}
+						>
 							Blockchain
 						</button>
 
-						<button className="Domains" value="Automation" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Automation"
+							onClick={() => {
+								ClickMe("Automation");
+							  }}
+						>
 							Automation
 						</button>
-						<button className="Domains" value="Ethical Hacking" onClick="">
+						<button className="Domains"
+							State="0"
+							id="Ethical Hacking"
+							onClick={() => {
+								ClickMe("Ethical Hacking");
+							  }}
+						>
 							Ethical Hacking
 						</button>
 
-						<button className="Domains" value="AutoCAD" onClick="">
+						<button className="Domains"
+							State="0"
+							id="AutoCAD"
+							onClick={() => {
+								ClickMe("AutoCAD");
+							  }}
+						>
 							AutoCAD
 						</button>
 					</div>
 				</div>
 			</div>
-			<div className="Submission">
-				<button className="Sub Submit">Next</button>
+			<div className="Buttons">
+				<div className="Submission">
+					<button className="Sub Submit">Next</button>
+					<button className="Sub Save">Save</button>
+				</div>
 			</div>
 		</>
 	);
